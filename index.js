1300572b6f661250d5aea4d858c19d4f58bdc580
hyee0715 <hyee0715@naver.com>
2022-01-21 20:54:12 +0900

    로그아웃 기능

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
-
+const {auth} = require('./middleware/auth')
 const {User} = require("./models/User")
 
 //application/x-www-form-urlencoded 타입의 데이터를 가져온다.
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('Hello World! zz')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다.
@@ -37,7 +37,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   // 요청된 이메일을 데이터베이스에 있는지 찾는다.
   User.findOne({email: req.body.email}, (err, user) => {
@@ -57,15 +57,42 @@ app.post('/login', (req, res) => {
           user.generateToken((err, user) => {
             if (err) return res.status(400).send(err)
 
-            // 토큰을 저장한다. 어디에? 쿠키, 스토리지,,
+            // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지,,
             // 여기서는 쿠키에 저장한다.
             res.cookie("x_auth", user.token)
             .status(200)
             .json({loginSuccess: true, userId: user._id})
           })
-
       })
   })
+})
+
+// role 1 어드민 role 2 특정 부서 어드민
+// role 0 -> 일반유저 role 0이 아니면 관리자
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth : true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
