7a19880e14209adaac9fcc709f95939fc87d5851
hyee0715 <hyee0715@naver.com>
2022-01-21 19:01:26 +0900

    토큰 생성

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 //application/json 타입의 데이터를 가져온다.
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
@@ -32,6 +34,37 @@ app.post('/register', (req, res) => {
     return res.status(200).json({
       success:true
     })
+  })
+})
+
+app.post('/login', (req, res) => {
+
+  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+      // 요청된 이메일이 데이터베이스에 있다면 맞는 비밀번호인지 확인
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch)
+          return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+        
+          // 비밀번호까지 맞다면 토큰을 생성하기
+          user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err)
+
+            // 토큰을 저장한다. 어디에? 쿠키, 스토리지,,
+            // 여기서는 쿠키에 저장한다.
+            res.cookie("x_auth", user.token)
+            .status(200)
+            .json({loginSuccess: true, userId: user._id})
+          })
+
+      })
   })
 })
 
