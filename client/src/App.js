1b211e1a6819f330a817f744f37cd0d996315bb7
hyee0715 <hyee0715@naver.com>
2022-01-22 21:15:51 +0900

    로그인, 로그아웃, 회원가입, auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import Auth from './hoc.auth'
+import { auth } from './_actions/user_action';
 
 function App() {
   return (
@@ -17,9 +19,9 @@ function App() {
           of them to render at a time
         */}
          <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={auth(<LandingPage />, null)} />
+          <Route path="/login" element={auth(<LoginPage />, false)} />
+          <Route path="/register" element={auth(<RegisterPage />, false)} />
         </Routes>
       </div>
     </Router>
